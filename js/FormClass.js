4ab7933228640b4ac42a467abc09eb7df6927b2c
AJ-Tan <aldomin22@gmail.com>
2025-08-19 10:21:34 +0800

    Refactor: FormClass.js - converted the display for alert message from using animation fill mode forwards to a simple toggle of display. alert.scss - adjusted to cater the new implementation

--- js/FormClass.js
+++ js/FormClass.js
@@ -1,146 +1,148 @@
 export class Form {
-   constructor(formClass) {
-      this.form = document.querySelector(`.${formClass}`);
-      this.formData = Object.fromEntries(new FormData(this.form));
-   }
+  constructor(formClass) {
+    this.form = document.querySelector(`.${formClass}`);
+    this.formData = Object.fromEntries(new FormData(this.form));
+  }
 
-   init() {
-      this.initSubmit();
-      this.initTextInputs();
-      this.initRadioInputs();
-      this.initCheckboxInput();
-   }
+  init() {
+    this.initSubmit();
+    this.initTextInputs();
+    this.initRadioInputs();
+    this.initCheckboxInput();
+  }
 
-   initTextInputs() {
-      const textNodes = this.form.querySelectorAll('input:is([type="text"],[type="email"]), textarea');
- 
-      for(const node of textNodes) {
-         node.addEventListener('blur',() => {
-            this.updateFormData();
-            this.validateInput(node);
-         })
+  initTextInputs() {
+    const textNodes = this.form.querySelectorAll(
+      'input:is([type="text"],[type="email"]), textarea'
+    );
+
+    for (const node of textNodes) {
+      node.addEventListener("blur", () => {
+        this.updateFormData();
+        this.validateInput(node);
+      });
+    }
+  }
+
+  initRadioInputs() {
+    const radioNodes = this.form.querySelectorAll('input:is([type="radio"]');
+    for (const node of radioNodes) {
+      node.addEventListener("change", () => {
+        for (const node of radioNodes) {
+          this.updateFormData();
+          this.validateInput(node);
+        }
+      });
+    }
+  }
+
+  initCheckboxInput() {
+    const checkbox = this.form.querySelector('input:is([type="checkbox"]');
+    checkbox.addEventListener("change", () => {
+      this.updateFormData();
+      this.validateInput(checkbox);
+    });
+  }
+
+  initSubmit() {
+    const submitButton = document.querySelector(".form__input-submit");
+    const alertContainer = document.querySelector(".alert__container");
+
+    submitButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.updateFormData();
+      this.validateAllInputs();
+
+      const validInputs = this.allInputsValid();
+      if (validInputs) {
+        this.resetInputs();
+        window.scrollTo(0, 0);
+
+        alertContainer.style.display = "block";
       }
-   }
+    });
 
-   initRadioInputs(){
-      const radioNodes = this.form.querySelectorAll('input:is([type="radio"]');
-      for(const node of radioNodes) {
-         node.addEventListener('change', () => {
-            for(const node of radioNodes) {
-               this.updateFormData();
-               this.validateInput(node);
-            }
-         })
-      }
-   }
+    alertContainer.addEventListener("animationend", (e) => {
+      e.currentTarget.style.display = "none";
+    });
+  }
 
-   initCheckboxInput(){
-      const checkbox = this.form.querySelector('input:is([type="checkbox"]');
-      checkbox.addEventListener('change', () => {
-         this.updateFormData();
-         this.validateInput(checkbox);
-      })
-   }
+  updateFormData() {
+    this.formData = Object.fromEntries(new FormData(this.form));
+  }
 
-   initSubmit() {
-      const submitButton = document.querySelector('.form__input-submit');
+  allInputsValid() {
+    const inputNodes = [...this.getAllInputNodes()];
 
-      submitButton.addEventListener('click', (e) => {
-         e.preventDefault();
-         this.updateFormData();
-         this.validateAllInputs();
-         
-         const validInputs = this.allInputsValid();
-         if(validInputs) {
-            /* To clear animation-fill forwards */
-            const alertContainer = document.querySelector('.alert__container');
-            alertContainer.style.setProperty('animation', 'none');
-            /************************************/
+    return (
+      inputNodes.filter((node) => node.getAttribute("aria-invalid") === "true")
+        .length === 0
+    );
+  }
 
-            this.resetInputs();
-            window.scrollTo(0,0);
+  resetInputs() {
+    const inputNodes = this.getAllInputNodes();
 
-            /* Trigger animation upon submit */
-            alertContainer.style.setProperty('display', 'block');
-            alertContainer.style.setProperty('animation', 'alert 5s ease-in-out forwards');
-            /************************************/
-         }
-      })
-   }
-
-   updateFormData() {
-      this.formData = Object.fromEntries(new FormData(this.form));
-   }
-
-   allInputsValid() {
-      const inputNodes = [...this.getAllInputNodes()];
-
-      return inputNodes.filter(node => node.getAttribute('aria-invalid') === 'true').length === 0;
-   }
-
-   resetInputs() {
-      const inputNodes = this.getAllInputNodes();
-      
-      for(const node of inputNodes) {
-         const inputType = node.getAttribute('type') || node.tagName.toLowerCase();
-         switch (inputType) {
-            case 'text':
-            case 'textarea':
-            case 'email':
-               node.value = '';
-
-               break;
-            case 'radio':
-            case 'checkbox':
-               node.checked = false;
-
-               break;
-            default:
-               throw error('Invalid input', node);
-         }
-      }
-   }
-
-   getAllInputNodes() {
-      return this.form.querySelectorAll('input, textarea');
-   }
-
-   validateAllInputs() {
-      const inputNodes = this.getAllInputNodes();
-      
-      for(const node of inputNodes) {
-         this.validateInput(node);
-      }
-   }
-
-   validateInput(node) {
-      const inputType = node.getAttribute('type') || node.tagName.toLowerCase();
-      const inputNameAttr = node.getAttribute('name');
-
-      node.setAttribute('aria-invalid','false');
+    for (const node of inputNodes) {
+      const inputType = node.getAttribute("type") || node.tagName.toLowerCase();
       switch (inputType) {
-         case 'text':
-         case 'textarea':
-            if(node.value === "") {
-               node.setAttribute('aria-invalid','true');
-            }
+        case "text":
+        case "textarea":
+        case "email":
+          node.value = "";
 
-            break;
-         case 'email':
-            if(!node.checkValidity()) {
-               node.setAttribute('aria-invalid','true');
-            }
-            
-            break;
-         case 'radio':
-         case 'checkbox':
-            if(!this.formData[inputNameAttr]){
-               node.setAttribute('aria-invalid','true');
-            }
+          break;
+        case "radio":
+        case "checkbox":
+          node.checked = false;
 
-            break;
-         default:
-            throw error('Invalid input', node);
+          break;
+        default:
+          throw error("Invalid input", node);
       }
-   }
+    }
+  }
+
+  getAllInputNodes() {
+    return this.form.querySelectorAll("input, textarea");
+  }
+
+  validateAllInputs() {
+    const inputNodes = this.getAllInputNodes();
+
+    for (const node of inputNodes) {
+      this.validateInput(node);
+    }
+  }
+
+  validateInput(node) {
+    const inputType = node.getAttribute("type") || node.tagName.toLowerCase();
+    const inputNameAttr = node.getAttribute("name");
+
+    node.setAttribute("aria-invalid", "false");
+    switch (inputType) {
+      case "text":
+      case "textarea":
+        if (node.value === "") {
+          node.setAttribute("aria-invalid", "true");
+        }
+
+        break;
+      case "email":
+        if (!node.checkValidity()) {
+          node.setAttribute("aria-invalid", "true");
+        }
+
+        break;
+      case "radio":
+      case "checkbox":
+        if (!this.formData[inputNameAttr]) {
+          node.setAttribute("aria-invalid", "true");
+        }
+
+        break;
+      default:
+        throw error("Invalid input", node);
+    }
+  }
 }
